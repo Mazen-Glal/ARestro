3e0d9136599b23472e1b073e19f9a72a2ed427fa
Mazen-Glal <mazenglal19@gmail.com>
2024-12-26 17:45:04 +0200

    add colors that used in the app..

---
+++ lib/core/utils/customs/assets.dart
@@ -0,0 +1,8 @@
+class Assets{
+  static const String namedWhiteLogo = 'assets/logo/name_logo_white.svg';
+  static const String namedBlackLogo = 'assets/logo/name_logo_black.svg';
+  static const String logoWithoutName = 'assets/logo/logo_without_name.svg';
+  static const String stampLight = 'assets/logo/Stamp_Light.svg';
+  static const String stampDark = 'assets/logo/Stamp_Dark.svg';
+
+}
---
+++ lib/core/utils/customs/colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColors{
+  static const Color greenWhite = Color(0xffBBFFDD) ;
+  static const Color green = Color(0xff009944) ;
+  static const Color greenBlack = Color(0xff004422) ;
+  static const Color white = Color(0xffFFFFFF) ;
+  static const Color black = Color(0xff000000) ;
+  static const Color halfBlack = Color(0xff707070) ;
+  // static const Color whiteGreen = Color(0xffAA1234) ;
+}
